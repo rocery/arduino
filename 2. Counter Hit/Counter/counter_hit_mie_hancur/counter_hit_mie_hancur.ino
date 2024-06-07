75ebdd2cc58476fe64af79e41f0911d4f7284cc8
rocery <sastranuralamsyah@gmail.com>
2024-06-07 11:28:44 +0700

    minimize code

--- 2. Counter Hit/Counter/counter_hit_mie_hancur/counter_hit_mie_hancur.ino
+++ 2. Counter Hit/Counter/counter_hit_mie_hancur/counter_hit_mie_hancur.ino
@@ -53,14 +53,14 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 ESP32 akan memilih WiFi dengan sinyal paling kuat secara otomatis
 */
 WiFiMulti wifiMulti;
+const char* ssid_a = "STTB8";
+const char* password_a = "siantar123";
 const char* ssid_d = "STTB1";
 const char* password_d = "Si4nt4r321";
 const char* ssid_b = "MT1";
 const char* password_b = "siantar321";
 const char* ssid_c = "MT3";
 const char* password_c = "siantar321";
-const char* ssid_a = "STTB8";
-const char* password_a = "siantar123";
 const char* ssid_it = "Tester_ITB";
 const char* password_it = "Si4nt4r321";
 
@@ -102,13 +102,20 @@ String postData;
 bool menuSelect;
 int menu = 1;
 String productSelected, nameProductSelected;
-String productCodeOne = "P-0722-00245", String nameProductOne = "SP MG BBQ";
-String productCodeTwo = "P-0722-00247", String nameProductTwo = "SP MG Smbl Balado";
-String productCodeThree = "P-0124-00287", String nameProductThree = "SP SB Mie Chicken";
-String productCodeFour = "P-0124-00285", String nameProductFour = "SP SB Mie Potato";
-String productCodeFive = "P-0124-00286", String nameProductFive = "SP SB Mie Balado";
-String productCodeSix = "P-0722-00246", String nameProductSix = "SK Mie Ayam Kecap";
-String productCodeSeven = "Test 219 Mie Hancur", String nameProductSeven = "Test Mode_219";
+String productCodeOne = "P-0722-00245";
+String nameProductOne = "SP MG BBQ";
+String productCodeTwo = "P-0722-00247";
+String nameProductTwo = "SP MG Smbl Balado";
+String productCodeThree = "P-0124-00287";
+String nameProductThree = "SP SB Mie Chicken";
+String productCodeFour = "P-0124-00285";
+String nameProductFour = "SP SB Mie Potato";
+String productCodeFive = "P-0124-00286";
+String nameProductFive = "SP SB Mie Balado";
+String productCodeSix = "P-0722-00246";
+String nameProductSix = "SK Mie St Aym Kcp";
+String productCodeSeven = "Test 219 Mie Hancur";
+String nameProductSeven = "Test Mode_219";
 
 // == SD Card ==
 int lineAsInt;
