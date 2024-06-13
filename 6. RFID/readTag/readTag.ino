71855740713c117329582b8e7f9b430591c2ceb9
rocery <sastranuralamsyah@gmail.com>
2024-06-13 09:23:19 +0700

    update

--- 6. RFID/readTag/readTag.ino
+++ 6. RFID/readTag/readTag.ino
@@ -5,25 +5,21 @@
 
 PN532_I2C pn532_i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532_i2c);
+String payloadAsString, tagId;
 
-void setup(void) {
-  Serial.begin(115200);
-  Serial.println("NFC Tag Reader");
-  nfc.begin();
-}
-
-void loop(void) {
-  Serial.println("\nPlace your NFC tag near the reader...");
-  if (nfc.tagPresent()) {
+bool readRFID() {
+  payloadAsString = "";
+  Serial.println("\nLetakan kartu RFID/NFC pada sensor");
+  if (nfc.tagPresent(20)) {
     NfcTag tag = nfc.read();
-    Serial.println("NFC Tag found:");
-    Serial.print("Tag Type: ");
-    Serial.println(tag.getTagType());
-    Serial.print("UID: ");
-    Serial.println(tag.getUidString());
-    tag.print();
+    // Serial.println("NFC Tag found:");
+    // // Serial.print("Tag Type: ");
+    // // Serial.println(tag.getTagType());
+    // Serial.print("UID: ");
+    tagId = tag.getUidString();
+    // tag.print();
 
-    if (tag.hasNdefMessage()) {  // Check if the tag has an NDEF message
+    if (tag.hasNdefMessage()) {
       NdefMessage message = tag.getNdefMessage();
       Serial.print("Message: ");
       for (int i = 0; i < message.getRecordCount(); i++) {
@@ -32,17 +28,31 @@ void loop(void) {
         byte payload[payloadLength];
         record.getPayload(payload);
 
-        // Assuming the payload is ASCII text
-        String payloadAsString = "";
         for (int c = 1; c < payloadLength; c++) {
           payloadAsString += (char)payload[c];
         }
         Serial.println(payloadAsString);
-        if (payloadAsString == "ACC") {
-          Serial.println("Success");
-        }
       }
     }
+
+    return true;
+  } else {
+    Serial.println("\nKatru RFID/NFC tidak terbaca");
+    return false;
   }
-  delay(1000);
+}
+
+void setup(void) {
+  Serial.begin(115200);
+  Serial.println("NFC Tag Reader");
+  nfc.begin();
+}
+
+void loop(void) {
+  readRFID();
+  if (payloadAsString == "ACC" || payloadAsString == "enACC") {
+    Serial.println("OKE");
+  } else {
+    Serial.println("FALSE");
+  }
 }
