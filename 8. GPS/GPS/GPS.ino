0cdac278eeffe615bc64479e3baa66e91d5bfc84
rocery <sastranuralamsyah@gmail.com>
2024-06-26 14:25:16 +0700

    init GPS project

---
+++ 8. GPS/GPS/GPS.ino
@@ -0,0 +1,28 @@
+/*
+  V. 0.0.1 Alpa
+  Update Terakhir : 26-06-2024
+
+  Komponen:
+  1. ESP32 Doit V1
+  2. SIM 800L
+  3. GPS Neo-8M
+
+  Algoritma:
+  Setup:
+    Setup SIM -> Setup GPS
+  Main:
+    Pembacaan GPS -> Waktu kirim ? pembacaan GPS, kirim data
+*/
+
+// ============ INISIALISASI LIBRARY ============
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
