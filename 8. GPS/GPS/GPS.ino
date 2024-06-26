6dab268cc7ed32bc4d425f67ae32b81edb4e51f8
rocery <sastranuralamsyah@gmail.com>
2024-06-26 15:06:25 +0700

    add sim init

--- 8. GPS/GPS/GPS.ino
+++ 8. GPS/GPS/GPS.ino
@@ -15,14 +15,39 @@
 */
 
 // ============ INISIALISASI LIBRARY ============
+#include <HardwareSerial.h>
+#include <TinyGPS++.h>
 
+// ============ GPS ============
+TinyGPSPlus gps;
+
+// ============ Hardware Serial ============
+// Gunakan Hardware Serial nomor 2 pada ESP32 yaitu pin 16, 17
+HardwareSerial simSerial(2);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
 
+  // Inialisasi Hardware Serial yang digunakan SIM800L (RX, TX)
+  // Pin RX SIM800L => 17, Pin TX SIM800L => 16
+  simSerial.begin(9600, SERIAL_8N1, 16, 17);
+  sim800Linit();
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void sim800lInit() {
+  sendCommand("AT", 1000, "OK");
+  sendCommand("AT+CPIN?", 1000, "READY");
+  sendCommand("AT+CREG?", 1000, "OK");
+  sendCommand("AT+CGATT?", 1000, "OK");
+  sendCommand("AT+CIPSHUT", 1000, "SHUT OK");
+  sendCommand("AT+CIPMUX=0", 1000, "OK");
+  sendCommand("AT+CSTT=\"your_apn\",\"your_username\",\"your_password\"", 1000, "OK");
+  sendCommand("AT+CIICR", 1000, "OK");
+  sendCommand("AT+CIFSR", 1000, ".");
 }
