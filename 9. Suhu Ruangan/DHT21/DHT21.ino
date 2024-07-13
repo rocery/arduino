a11e3d4bb1e0ecfb27b6bdcaf3b79ca49e359358
rocery <sastranuralamsyah@gmail.com>
2024-07-13 11:58:29 +0700

    update dht

--- 9. Suhu Ruangan/DHT21/DHT21.ino
+++ 9. Suhu Ruangan/DHT21/DHT21.ino
@@ -1,25 +1,30 @@
-
-
 #include <DHT.h>
 
 #define DHTPIN 4
 #define DHTTYPE DHT21
 
+float temperature, humidity;
+
+void readDHT() {
+    temperature = dht.getTemperature();
+    humidity = dht.getHumidity();
+}
+
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-    Serial.begin(115200);
-    dht.begin();
+  Serial.begin(115200);
+  dht.begin();
 }
 
 void loop() {
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
-    Serial.print("Humidity: ");
-    Serial.print(h);
-    Serial.print(" %\t");
-    Serial.print("Temperature: ");
-    Serial.print(t);
-    Serial.println(" *C ");
-    delay(2000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+  delay(2000);
 }
