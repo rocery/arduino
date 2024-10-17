40cc26150a4f93aaa85fc1c8579c77939c5a3202
rocery <sastranuralamsyah@gmail.com>
2024-10-17 14:53:18 +0700

    update new counter

--- 2. Counter Hit/Counter/potato/potato.ino
+++ 2. Counter Hit/Counter/potato/potato.ino
@@ -7,7 +7,7 @@
   1. Micro Controller : ESP32
   2. LCD I2C 20x4                               (3.3v, GND, I2C (22 = SCL, 21 = SDA))
   3. RTC DS3231                                 (3.3v, GND, I2C (22 = SCL, 21 = SDA))
-  4. IR Sensor                                  (5v/Vin, GND, 13)
+  4. IR Sensor                                  (5v/Vin, GND, 27)
   5. Module SD Card + SD Card (FAT32 1-16 GB)   (3.3v, GND, SPI(Mosi 23, Miso 19, CLK 18, CS 5))
   6. Tacticle Button 1x1 cm @3                  (3.3v, GND, 34, 35, 25)
   -- Belum diimplementasikan --
