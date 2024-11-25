ad0f29d42eae0888135a9a66758fd759fe9dd90f
rocery <sastranuralamsyah@gmail.com>
2024-11-25 10:52:18 +0700

    begin weigher

--- 3. Weigher/Weigher/Weigher.ino
+++ 3. Weigher/Weigher/Weigher.ino
@@ -1,78 +1,37 @@
-/*
- * Complete project details at https://RandomNerdTutorials.com/esp32-load-cell-hx711/
- *
- * HX711 library for Arduino - example file
- * https://github.com/bogde/HX711
- *
- * MIT License
- * (c) 2018 Bogdan Necula
- *
-**/
-#include <Arduino.h>
-#include "HX711.h"
-#include "soc/rtc.h"
+#include <HX711.h>
 
-// HX711 circuit wiring
-const int LOADCELL_DOUT_PIN = 16;
-const int LOADCELL_SCK_PIN = 4;
-
+// HX711 Wiring
+const int LOADCELL_DOUT_PIN = 27;
+const int LOADCELL_SCK_PIN = 26;
 HX711 scale;
 
+#define buttonUp 34
+#define buttonDown 35
+#define buttonSelect 32
+
 void setup() {
-  Serial.begin(115200);
-  rtc_cpu_freq_config_t config;
-  rtc_clk_cpu_freq_get_config(&config);
-  rtc_clk_cpu_freq_to_config(RTC_CPU_FREQ_80M, &config);
-  rtc_clk_cpu_freq_set_config_fast(&config);
-  Serial.println("HX711 Demo");
-
-  Serial.println("Initializing the scale");
-
+  Serial.begin(9600);
+  
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  
+  pinMode(buttonUp, INPUT_PULLUP);
+  pinMode(buttonDown, INPUT_PULLUP);
+  pinMode(buttonSelect, INPUT_PULLUP);
 
-  Serial.println("Before setting up the scale:");
-  Serial.print("read: \t\t");
-  Serial.println(scale.read());      // print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scale.read_average(20));   // print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight (not set yet)
-
-  Serial.print("get units: \t\t");
-  Serial.println(scale.get_units(5), 1);  // print the average of 5 readings from the ADC minus tare weight (not set) divided
-            // by the SCALE parameter (not set yet)
-            
-  scale.set_scale(INSERT YOUR CALIBRATION FACTOR);
-  //scale.set_scale(-471.497);                      // this value is obtained by calibrating the scale with known weights; see the README for details
-  scale.tare();               // reset the scale to 0
-
-  Serial.println("After setting up the scale:");
-
-  Serial.print("read: \t\t");
-  Serial.println(scale.read());                 // print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight, set with tare()
-
-  Serial.print("get units: \t\t");
-  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
-            // by the SCALE parameter set with set_scale
-
-  Serial.println("Readings:");
+  scale.tare();
 }
 
 void loop() {
-  Serial.print("one reading:\t");
-  Serial.print(scale.get_units(), 1);
-  Serial.print("\t| average:\t");
-  Serial.println(scale.get_units(10), 5);
+  if (scale.is_ready()) {
+    long reading = scale.get_units(10);
+    Serial.print("Hasil : ");
+    Serial.println(reading);
+  } else {
+    Serial.println("HX711 not found.");
+  }
+  delay(1000);
+}
 
-  scale.power_down();             // put the ADC in sleep mode
-  delay(5000);
-  scale.power_up();
+bool isButtonPressed(int buttonPin) {
+  return !digitalRead(buttonPin);
 }
