a4e32200da8945179a45da1ab140d0d14cfe422f
rocery <sastranuralamsyah@gmail.com>
2024-12-05 13:18:11 +0700

    a

--- 3. Weigher/weigher_eth/weigher_eth.ino
+++ 3. Weigher/weigher_eth/weigher_eth.ino
@@ -84,7 +84,240 @@ void updateFloatInEEPROM(int address, float newValue) {
   }
 }
 
+bool isButtonPressed(int buttonPin) {
+  return digitalRead(buttonPin);
+}
 
+void tareScale() {
+  scale.set_scale(calibrationFactor);
+  scale.tare();
+  lcd.clear();
+}
+
+void kirimDataKeServer() {
+  EthernetClient client;
+  
+  // Coba koneksi
+  if (client.connect(serverAddress, serverPort)) {
+    Serial.println("Terhubung ke server");
+    
+    // Kirim HTTP POST Request
+    client.println("POST /iot/api/weigher/save_weigher_test.php HTTP/1.1");
+    client.println("Host: 192.168.7.223");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Connection: close");
+    client.print("Content-Length: ");
+    client.println(postData.length());
+    client.println();
+    client.println(postData);
+    
+    Serial.println("Data terkirim:");
+    Serial.println(postData);
+    
+    // Tunggu respon
+    while(client.connected()) {
+      if(client.available()) {
+        char c = client.read();
+        Serial.print(c);
+      }
+    }
+    
+    client.stop();
+  } else {
+    Serial.println("Koneksi gagal");
+  }
+}
+
+void calibrationProcess() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("  BERAT BARANG  ");
+
+  int currentValueIndex = 0;
+  int currentDigitIndex = 0;
+
+  // Step 1: Select weight
+  while (true) {
+    lcd.setCursor(0, 1);
+    lcd.print("BERAT: ");
+    lcd.print(values[currentValueIndex]);
+    lcd.print(" KG");
+
+    int buttonUpState = digitalRead(buttonUp);
+    int buttonDownState = digitalRead(buttonDown);
+
+    // If buttonUp is pressed
+    if (buttonUpState == HIGH) {
+      while (digitalRead(buttonUp) == HIGH)
+        ;
+      currentValueIndex = (currentValueIndex + 1) % ARRAY_LENGTH(values);
+      delay(200);
+    }
+
+    // If buttonDown is pressed, move to the next step
+    if (buttonDownState == HIGH) {
+      while (digitalRead(buttonDown) == HIGH)
+        ;
+      break;
+    }
+  }
+
+  // Step 2: Empty the scale
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("KOSONGKAN");
+  lcd.setCursor(0, 1);
+  lcd.print("TIMBANGAN");
+  int timerStep2 = 3;
+  for (int i = 0; i <= timerStep2; i++) {
+    lcd.setCursor(12, 0);
+    lcd.print(timerStep2 - i);
+    // scale.set_scale(0);
+    scale.tare();
+    delay(600);
+  }
+  lcd.setCursor(11, 1);
+  lcd.print("OKE");
+  // Wait for user to empty the scale
+  while (true) {
+    int buttonDownState = digitalRead(buttonDown);
+
+    // If buttonDown is pressed, move to the next step
+    if (buttonDownState == HIGH) {
+      while (digitalRead(buttonDown) == HIGH)
+        ;
+      break;
+    }
+  }
+
+  // Step 3: Weigh the item
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(" TIMBANG BARANG");
+  lcd.setCursor(1, 1);
+  lcd.print(values[currentValueIndex]);
+  lcd.print(" KG");
+  delay(1000);
+
+  while (true) {
+    // Tare the scale and get the reading
+    long reading = scale.get_units(10);
+    calibrationFactor = (float)reading / (values[currentValueIndex] * 1000);
+    lcd.setCursor(10, 1);
+    lcd.print(calibrationFactor);
+    scale.power_down();
+    delay(1000);
+    scale.power_up();
+
+    int buttonDownState = digitalRead(buttonDown);
+
+    // If buttonDown is pressed, move to the next step
+    if (buttonDownState == HIGH) {
+      lcd.clear();
+      while (digitalRead(buttonDown) == HIGH) {
+        lcd.setCursor(0, 0);
+        lcd.print(" DATA DISIMPAN ");
+      }
+      lcd.clear();
+      break;
+    }
+  }
+
+  // Step 4: Set Digit
+  while (true) {
+    lcd.setCursor(0, 0);
+    lcd.print("    SET DIGIT   ");
+    lcd.setCursor(0, 1);
+    lcd.print("DIGIT: ");
+    lcd.print(digit[currentDigitIndex]);
+
+    int buttonUpState = digitalRead(buttonUp);
+    int buttonDownState = digitalRead(buttonDown);
+
+    // If buttonUp is pressed
+    if (buttonUpState == HIGH) {
+      while (digitalRead(buttonUp) == HIGH)
+        ;
+      currentDigitIndex = (currentDigitIndex + 1) % ARRAY_LENGTH(digit);
+      delay(200);
+    }
+
+    // If buttonDown is pressed, move to the next step
+    if (buttonDownState == HIGH) {
+      while (digitalRead(buttonDown) == HIGH)
+        ;
+      digitScale = digit[currentDigitIndex];
+      break;
+    }
+  }
+
+  // Step 5: Confirm calibration
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("KALIBRASI");
+  lcd.print("  H <==");
+  lcd.setCursor(0, 1);
+  lcd.print("SELESAI");
+  lcd.print("    M ==>");
+
+  while (true) {
+    int buttonSelectState = digitalRead(buttonSelect);
+    int buttonDownState = digitalRead(buttonDown);
+
+    // If buttonSelect is pressed, exit the calibration process
+    if (buttonSelectState == HIGH) {
+      while (digitalRead(buttonSelect) == HIGH)
+        ;
+      updateFloatInEEPROM(EEPROM_ADDRESS, calibrationFactor);
+      updateFloatInEEPROM(EEPROM_ADDRESS + 4, digitScale);
+      lcd.clear();
+      return;
+    }
+
+    // If buttonDown is pressed, restart the calibration
+    if (buttonDownState == HIGH) {
+      while (digitalRead(buttonDown) == HIGH)
+        ;
+      calibrationProcess();
+      break;
+    }
+  }
+}
+
+void chooseProduct() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("  PILIH PRODUK ");
+
+  int currentProductIndex = 0;
+
+  // Step 1: Select weight
+  while (true) {
+    lcd.setCursor(0, 1);
+    lcd.print(product[currentProductIndex]);
+
+    int buttonUpState = digitalRead(buttonUp);
+    int buttonDownState = digitalRead(buttonDown);
+
+    // If buttonUp is pressed
+    if (buttonUpState == HIGH) {
+      lcd.setCursor(0, 1);
+      lcd.print("                ");
+      while (digitalRead(buttonUp) == HIGH)
+        ;
+      currentProductIndex = (currentProductIndex + 1) % ARRAY_LENGTH(product);
+      delay(200);
+    }
+
+    // If buttonDown is pressed, move to the next step
+    if (buttonDownState == HIGH) {
+      while (digitalRead(buttonDown) == HIGH)
+        ;
+      productSelected = product[currentProductIndex];
+      break;
+    }
+  }
+}
 
 void setup() {
   Serial.begin(9600);
