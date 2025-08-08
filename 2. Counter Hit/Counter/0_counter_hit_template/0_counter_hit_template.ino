d1c2cc53030a5a0e0b38310489e3c2936feb11b8
rocery <sastranuralamsyah@gmail.com>
2025-08-08 10:49:41 +0700

    ???

--- 2. Counter Hit/Counter/0_counter_hit_template/0_counter_hit_template.ino
+++ 2. Counter Hit/Counter/0_counter_hit_template/0_counter_hit_template.ino
@@ -2,6 +2,8 @@
   V. 1.1.0
   Update Terakhir : 13-01-2025
 
+  Versi Board ESP32 : V. 3.1.1
+
   PENTING = Harus menggunakan Dual Core Micro Controller/Microprocessor
   Komponen:
   1. Micro Controller : ESP32
@@ -10,12 +12,6 @@
   4. IR Sensor                                  (5v/Vin, GND, 13)
   5. Module SD Card + SD Card (FAT32 1-16 GB)   (3.3v, GND, SPI(Mosi 23, Miso 19, CLK 18, CS 5))
   6. Tacticle Button 1x1 cm @3                  (3.3v, GND, 34, 35, 25)
-  -- Belum diimplementasikan --
-  7. Active Buzzer 3-5 v                        (3.3v, 26)
-  8. Fan 5V                                     (5v/Vin, GND)
-
-  Program ini berfungsi untuk melakukan penghitungan barang pada conveyor.
-  Penjelasan program terdapat pada comment baris pada program.
 
   Semua fungsi Serial.print() pada program ini sebenarnya bisa dihapus/di-comment,
   masih dipertahankan untuk fungsi debuging. Akan di-comment/dihapus pada saat final
@@ -23,3 +19,4 @@
   
 */
 
+// 
