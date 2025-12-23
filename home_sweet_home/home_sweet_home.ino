9dafd4c6efb39ec9c32a6964bd70c6262a9f40a4
rocery <sastranuralamsyah@gmail.com>
2025-12-23 16:19:55 +0700

    update: add initial implementation of home automation with DHT sensor and web server

---
+++ home_sweet_home/home_sweet_home.ino
@@ -0,0 +1,122 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <DHT.h>
+
+// ================== SENSOR ==================
+#define DHTPIN 33
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+#define LED 2
+
+// ================== WIFI ==================
+const char* ssid = "STTB11";
+const char* password = "Si4nt4r321";
+
+IPAddress local_IP(192, 168, 15, 230);
+IPAddress gateway(192, 168, 15, 250);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress dns1(8, 8, 8, 8);
+IPAddress dns2(8, 8, 4, 4);
+
+// ================== SERVER ==================
+WebServer server(80);
+
+// ================== HTML BUILDER ==================
+String buildHTML(float temp, float hum) {
+  String html = "";
+
+  html += "<!DOCTYPE html>";
+  html += "<html>";
+  html += "<head>";
+  html += "<meta name='viewport' content='width=device-width, initial-scale=1'>";
+  html += "<meta http-equiv='refresh' content='3'>";
+  html += "<style>";
+
+  html += "body{font-family:Arial;background:#0f172a;color:#e5e7eb;text-align:center;}";
+  html += ".card{background:#1e293b;padding:25px;margin:40px auto;width:480px;";
+  html += "border-radius:12px;box-shadow:0 0 20px rgba(0,0,0,0.5);}";
+
+  html += "h1{color:#237599;}";
+  html += ".value{font-size:32px;font-weight:bold;}";
+  html += ".label{font-size:21px;color:#94a3b8;}";
+  html += ".ip{font-size:15px;color:#94a3b8;}";
+
+  html += ".parent {display: grid;grid-template-columns: repeat(4, 1fr);grid-template-rows: repeat(5, 1fr);gap: 8px;}";
+
+  html += ".div1{grid-column: span 2 / span 2;grid-row: span 5 / span 5;}";
+  html += ".div2{grid-column: span 2 / span 2;grid-row: span 5 / span 5;grid-column-start: 3;}";
+
+  html += "</style>";
+  html += "</head>";
+
+  html += "<body>";
+  html += "<div class='card'>";
+  html += "<h1>ESP32 DHT22</h1>";
+
+  html += "<div class='parent'>";
+
+  html += "<div class='div1'>";
+  html += "<p class='label'>Temperature</p>";
+  html += "<p class='value'>" + String(temp, 1) + " &deg;C</p>";
+  html += "</div>";
+
+  html += "<div class='div2'>";
+  html += "<p class='label'>Humidity</p>";
+  html += "<p class='value'>" + String(hum, 1) + " %</p>";
+  html += "</div>";
+
+  html += "</div>";
+
+  html += "<p class='ip'>" + WiFi.localIP().toString() + "</p>";
+  html += "</div>";
+  html += "</body>";
+  html += "</html>";
+
+  return html;
+}
+
+// ================== ROUTE ==================
+void handleRoot() {
+
+  // Blink LED ON refresh
+  // digitalWrite(LED, HIGH);
+  // delay(50);
+  digitalWrite(LED, LOW);
+  delay(100);
+
+  float calTemp = -0.5;
+  float calHum = 0.0;
+  float temp = dht.readTemperature() + calTemp;
+  float hum = dht.readHumidity() + calHum;
+
+  if (isnan(temp) || isnan(hum)) {
+    server.send(500, "text/plain", "Sensor read error");
+    return;
+  }
+
+  server.send(200, "text/html", buildHTML(temp, hum));
+}
+
+// ================== SETUP ==================
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+
+  pinMode(LED,OUTPUT);
+
+  WiFi.config(local_IP, gateway, subnet, dns1, dns2);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  server.on("/", handleRoot);
+  server.begin();
+}
+
+// ================== LOOP ==================
+void loop() {
+  server.handleClient();
+  digitalWrite(LED, HIGH);
+}
