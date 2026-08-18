1d7939d89aaaae660676111f75ced2ac97d9cb77
rocery <sastranuralamsyah@gmail.com>
2026-08-18 14:40:40 +0700

    update device configuration and network settings in template

--- 5. Downtime/Template/Template.ino
+++ 5. Downtime/Template/Template.ino
@@ -16,5 +16,26 @@
 #define PZEM_ADDRESS 0x14
 
 // Device Config
-#define DEVICE_NAME "Isi dengan nama device"
-#define DEVICE_ID 1
+#define DEVICE_NAME "Molding Line 4 - Biskuit"
+#define DEVICE_ID  194 // Isi dengan IP
+
+// WiFi Configuration
+const char* WIFI_NETWORKS[3][2] = {
+  {"STTB1", "Si4nt4r321"},
+  {"STTB2", "Si4nt4r321"},
+  {"MT3", "siantar321"}
+};
+const int WIFI_NETWORKS_COUNT = 3;
+
+// Network Configuration
+IPAddress STATIC_IP(192, 168, 7, DEVICE_ID);
+IPAddress GATEWAY(192, 168, 15, 250);
+IPAddress SUBNET(255, 255, 0, 0);
+IPAddress PRIMARY_DNS(8, 8, 8, 8);
+IPAddress SECONDARY_DNS(8, 8, 4, 4);
+
+// Server Configuration
+const char* API_HOST = "192.168.7.223";
+const char* API_STATUS_ENDPOINT = "/molding_api/saveStatus.php";
+const char* API_LOG_ENDPOINT = "/molding_api/createFile.php";
+const int API_PORT = 80;
